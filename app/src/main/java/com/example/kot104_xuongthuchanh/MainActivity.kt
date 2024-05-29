b5b03ad5402a50306155241bedbf695a2b5c2073
VuongwNguyen <nguyenvuongw134@gmail.com>
2024-05-29 22:12:29 +0700

    first commit

---
+++ app/src/main/java/com/example/kot104_xuongthuchanh/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.kot104_xuongthuchanh
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.kot104_xuongthuchanh.ui.theme.KOT104_XuongThucHanhTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            KOT104_XuongThucHanhTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    KOT104_XuongThucHanhTheme {
+        Greeting("Android")
+    }
+}
